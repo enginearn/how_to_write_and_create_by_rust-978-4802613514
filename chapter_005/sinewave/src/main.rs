6f78655a3ca0ad87907d348ae673a8ab14fe8ab9
enginearn <enginyaaaaan@gmail.com>
2022-10-12 07:58:57 +0900

    Added

--- chapter_005/sinewave/src/main.rs
+++ chapter_005/sinewave/src/main.rs
@@ -1,3 +1,22 @@
+use std::f32::consts::PI;
+use hound::WavWriter;
+
+const SAMPLE_RATE: u32 = 44100;
+const TONE: f32 = 440.0; // 440Hz = A
+
 fn main() {
-    println!("Hello, world!");
+    let spec = hound::WavSpec {
+        channels: 1,
+        sample_rate: SAMPLE_RATE,
+        bits_per_sample: 16,
+        sample_format: hound::SampleFormat::Int,
+    };
+    let mut fw = WavWriter::create("sine.wav", spec).unwrap();
+
+    let samples = SAMPLE_RATE * 3;
+    for t in 0..samples {
+        let v = ((t as f32 / SAMPLE_RATE as f32) *
+            TONE * 2.0 * PI).sin();
+    fw.write_sample((v * i16::MAX as f32) as i16).unwrap();
+    }
 }
